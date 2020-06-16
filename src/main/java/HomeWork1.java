3a43e935d17caf1b1bd3131f0407b1016cbd52f7
Solarci <solatci@gmail.com>
2020-06-16 20:36:33 +0300

    Homework 1

---
+++ src/main/java/HomeWork1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String line = scan.nextLine();
+        System.out.println("Enter your city");
+        String line2 = scan.nextLine();
+        System.out.println("Enter your age");
+        //scanner nextInt has minor bug and consumes 2 scans if after nextInt follows nextLine
+        int i1 = scan.nextInt();
+        scan.nextLine();
+        System.out.println("Enter your hobby");
+        String line3 = scan.nextLine();
+        System.out.println("Name:" + line);
+        System.out.println("City:" + line2);
+        System.out.println("Age:" + i1);
+        System.out.println("Hobby:" + line3);
+        System.out.println("Человек по имени " + line + " живёт в городе " + line2 + ". Ему " + i1 +
+                " лет и он любит заниматься " + line3 + ".");
+        System.out.println("\\\\"" + line +"\""+ " - name\n" +
+                line2 + " - city\n" +
+                i1 + " - age\n" +
+                line3 + " - hobby\n");
+    }
+}
